83ce8b9c7d5e27bf704ca8fdee00eab7651b1686
mkristian <m.kristian@web.de>
2010-07-22 20:59:25 +0530

    follow ixtlan changes

--- config/initializers/ixtlan.rb
+++ config/initializers/ixtlan.rb
@@ -1,41 +1,35 @@
 module Ixtlan
   module Models
-    AUTHENTICATION = "::Authentication"
-    USER = "::User"
-    GROUP = "::Group"
-    LOCALE = "::Locale"
-    DOMAIN = "::Domain"
-    TEXT = "::I18nText"
-    CONFIGURATION = "::Configuration"
+    # overwrite configuration class
+    # CONFIGURATION = "::MyConfiguration"
     # set this to nil to switch off Audit logs into the database
-    AUDIT = "::Audit"
+    # AUDIT = nil
   end
 end
-require 'ixtlan/modified_by'
-if ENV['RAILS_ENV']
-  require 'models'
-  require 'ixtlan/rails/error_handling'
-  require 'ixtlan/rails/audit'
-  require 'ixtlan/rails/session_timeout'
-  require 'ixtlan/rails/unrestful_authentication'
-  require 'ixtlan/rails/guard'
-  require 'ixtlan/rails/timestamps_modified_by_filter'
-  require 'ixtlan/optimistic_persistence'
-end
-require 'ixtlan/monkey_patches'
+
+# rails related libraries from ixtlan
+require 'ixtlan/rails'
 
 # auto require to load needed libraries . . .
 require 'datamapper4rails'
 require 'ixtlan/logger_config' if ENV['RAILS_ENV']
 
-# cleanup can be a problem. jruby uses soft-references for the cache so
-# memory cleanup with jruby is no problem.
 require 'ixtlan/session'
 ActionController::Base.session_store = :datamapper_store
-ActionController::Base.session = {
-  :cache       => true,
-  :session_class => Ixtlan::Session
-}
+
+if defined? JRUBY_VERSION || ENV['RAILS_ENV'] == 'development'
+  # jruby uses soft-references for the cache so cleanup is no problem.
+  ActionController::Base.session = {
+    :cache         => true,
+    :session_class => Ixtlan::SessionWithCache
+  }
+else
+  # cleanup can be a problem with MRI => no cache
+  ActionController::Base.session = {
+    :cache         => false,
+    :session_class => Ixtlan::Session
+  }
+end
 
 # load the guard config files from RAILS_ROOT/app/guards
 Ixtlan::Guard.load(Slf4r::LoggerFacade.new(Ixtlan::Guard)) if ENV['RAILS_ENV']
